b52901cef0c5345d92c9568be96ff031abda46d5
wented6 <44713416+wented6@users.noreply.github.com>
2018-11-11 01:34:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,25 +13,25 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('.wkDpdwDwl1l114llwss.')) {
+if (message.content.startsWith('wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.send(`${argresult} \` `)
 } else 
-if (message.content.startsWith('SeDAst21q.a')) {
+if (message.content.startsWith('lsn')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
     message.channel.send(` ${argresult} \` `)
 } else 
 if (message.content.startsWith('3tb')) {
   client.user.setGame(argresult, "https://www.twitch.tv/WE");
   client.user.setStatus("idle")
-   message.channel.send(`:white_check_mark:  \` `).then(msg => {msg.delete(3500);
+   message.channel.send(`:white_check_mark:  \` `)
 }
 });
 
 client.on('message', message => {
         if (!developers.includes(message.author.id)) return;
   if (message.content === 'owdoawowddwkowdokokwdokokk1o131141ds') {
-  let channel = client.channels.get('444873938691031040');
+  let channel = client.channels.get('510463644535095297');
 
   channel.join()
   .then(connection => console.log('Connected'))
